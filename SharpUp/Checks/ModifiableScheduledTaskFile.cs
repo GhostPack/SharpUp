b73b9972015a8bf6526793d2738f8664cb2d26b8
IceMoon <imcoeon@gmail.com>
2022-04-20 13:51:33 -0700

    Added check for task file. Updated output messages. Updated README

--- SharpUp/Checks/ModifiableScheduledTaskFile.cs
+++ SharpUp/Checks/ModifiableScheduledTaskFile.cs
@@ -19,8 +19,19 @@ namespace SharpUp.Checks
             string varName = Environment.GetEnvironmentVariable("SystemRoot");
             varName += IntPtr.Size == 8 ? "\\System32\\Tasks" : "\\SysWOW64\\Tasks";
             string[] allfiles = Directory.GetFiles(varName);
+
+            // Warning for Program Files
+            Console.WriteLine("[!] Warning: File paths containing Program Files could be false positives. Please check manually to confirm.");
+            
             foreach (string file in allfiles)
             {
+                // Check if task file is writable
+                bool taskperms = CheckAccess(file, FileSystemRights.Write);
+                if(taskperms == false)
+                {
+                    Console.WriteLine("[+] The current user has write permissions to {0}", file);
+                    Console.WriteLine();
+                }
                 try
                 {
                     // Load XML document
@@ -38,27 +49,15 @@ namespace SharpUp.Checks
                         if(writeperms == false)
                         {
                             string URIpath;
-                            string triggerlist;
-
                             // Get task name
                             XmlNodeList task = xmlDocument.GetElementsByTagName("URI");
                             for (int a = 0; a < task.Count; a++)
                             {
                                 URIpath = task[a].InnerXml;
-                                // Get task triggers
-                                XmlNodeList trigger = xmlDocument.GetElementsByTagName("Triggers");
-                                for (int b = 0; b < trigger.Count; b++)
-                                {
-                                    triggerlist = trigger[b].InnerXml;
-                                    Console.WriteLine("[+] Modifiable Task {0}.\n" +
-                                    "[+] Command executes {1}, which the current user can modify.", URIpath, commandpath
-                                    );
-                                    string path = commandpath.ToLower();
-                                    if(path.Contains("c:\\program files"))
-                                    {
-                                        Console.WriteLine("[!] Warning: File paths containing Program Files could be false positives. Please check manually to confirm.");
-                                    }
-                                }
+                                Console.WriteLine("[+] Modifiable Task {0}", URIpath);
+                                Console.WriteLine("[+] Command executes {0}", commandpath);
+                                Console.WriteLine("\tReason : User can modify command binary.");
+                                Console.WriteLine();
                             }
                         }
                     }
